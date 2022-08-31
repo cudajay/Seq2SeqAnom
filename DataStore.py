d960559a464f4db9818b0f9005f1bb15ad8d505c
cudajay <juanjose@smu.edu>
2022-08-31 11:58:27 -0500

    ADD Datastore class

---
+++ DataStore.py
@@ -0,0 +1,47 @@
+import os
+import glob
+import numpy as np
+
+MIN = 25
+MAX = 55
+
+
+class XY():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class DataStore():
+    def __init__(self, dataDir: os.PathLike, seqLength: int):
+        self.seqLength = seqLength
+        self.trainData = {}
+        self.testData = {}
+        self.__load(dataDir)
+
+    def __load(self, datadir: os.PathLike):
+        glb = glob.glob(os.path.join(datadir, "train", "*"))
+        for g in glb:
+            self.trainData[g.split("/")[-1].split(".")[0]] = self.__preprocess(np.load(g))
+
+        glb = glob.glob(os.path.join(datadir, "test", "*"))
+        for g in glb:
+            self.testData[g.split("/")[-1].split(".")[0]] = self.__preprocess(np.load(g))
+
+    def __preprocess(self, sample: np.array):
+        xs = []
+        ys = []
+        for i in range(sample.shape[0]):
+            if i + self.seqLength + 1 >= sample.shape[0]:  # X chunk + Y sequence
+                break
+            if sample.shape[1] == MIN:
+                x = np.concatenate((sample[i:i+self.seqLength], \
+                                    np.zeros((self.seqLength, MAX - MIN))), axis=1)
+                y = np.concatenate(([sample[i + self.seqLength]], \
+                                    np.zeros((1, MAX - MIN), )), axis=1)
+            else:
+                x = sample[i:i+self.seqLength]
+                y = sample[i + self.seqLength]
+            xs.append(x.reshape((MAX, self.seqLength)))
+            ys.append(y.reshape((MAX, 1)))
+        return XY(xs, ys)
