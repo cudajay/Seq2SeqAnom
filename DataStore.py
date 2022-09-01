a27ebbb635756b5bd4866ee2c05018de3c8adbe2
cudajay <juanjose@smu.edu>
2022-08-31 20:36:35 -0500

    Add labeler to test sequences

--- DataStore.py
+++ DataStore.py
@@ -2,15 +2,24 @@ import os
 import glob
 import numpy as np
 import random
-
+import pandas as pd
 MIN = 25
 MAX = 55
-
+def str2ary(str_):
+    x = str_.replace("]","").replace("[","")
+    x = x.split(",")
+    assert(not len(x)%2)
+    lst = []
+    for i in range(0, len(x), 2):
+        tmp = range(int(x[i]), int(x[i+1]))
+        lst.append(tmp)
+    return lst
 
 class XY():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.labels = []
     def shuffle(self):
         assert(len(self.x) == len(self.y))
         for i in len(self.x):
@@ -39,6 +48,9 @@ class DataStore():
         glb = glob.glob(os.path.join(datadir, "test", "*"))
         for g in glb:
             self.testData[g.split("/")[-1].split(".")[0]] = self.__preprocess(np.load(g))
+
+        for rw in pd.read_csv(os.path.join(datadir, "labeled_anomalies.csv")):
+            self.testData[rw[1]['chan_id']].labels = str2ary(rw[1]['anomaly_sequences'])
 
     def __preprocess(self, sample: np.array):
         xs = []
